5bbaeba9c5f88e8a90062c9108438d83c2950d7f
Mat Schaffer <mat@schaffer.me>
2015-05-25 15:52:15 +0900

    Move copy-pasted docker bits into base class

--- lib/stellar_core_commander/docker_process.rb
+++ lib/stellar_core_commander/docker_process.rb
@@ -6,22 +6,6 @@ module StellarCoreCommander
   class DockerProcess < Process
     include Contracts
 
-    attr_reader :working_dir
-    attr_reader :base_port
-    attr_reader :identity
-    attr_reader :server
-
-    def initialize(working_dir, base_port, identity)
-      @working_dir = working_dir
-      @base_port   = base_port
-      @identity    = identity
-
-      @server = Faraday.new(url: "http://#{docker_host}:#{http_port}") do |conn|
-        conn.request :url_encoded
-        conn.adapter Faraday.default_adapter
-      end
-    end
-
     Contract None => Any
     def launch_state_container
       run_cmd "docker", %W(run --name #{state_container_name} -p #{postgres_port}:5432 --env-file stellar-core.env -d stellar/stellar-core-state)
@@ -42,11 +26,6 @@ module StellarCoreCommander
     end
 
     Contract None => Any
-    def rm_working_dir
-      FileUtils.rm_rf working_dir
-    end
-
-    Contract None => Any
     def setup
       write_config
       launch_state_container
@@ -58,24 +37,6 @@ module StellarCoreCommander
       launch_stellar_core
     end
 
-
-    Contract None => Any
-    def wait_for_ready
-      loop do
-
-        response = server.get("/info") rescue false
-
-        if response
-          body = ActiveSupport::JSON.decode(response.body)
-
-          break if body["info"]["state"] == "Synced!"
-        end
-
-        $stderr.puts "waiting until stellar-core is synced"
-        sleep 1
-      end
-    end
-
     Contract None => Bool
     def running?
       run_cmd "docker", %W(inspect #{container_name})
@@ -85,62 +46,7 @@ module StellarCoreCommander
     Contract None => Any
     def shutdown
       return true unless running?
-
       run_cmd "docker", %W(rm -f #{container_name})
-    end
-
-    Contract None => Bool
-    def close_ledger
-      prev_ledger = latest_ledger
-      next_ledger = prev_ledger + 1
-
-      server.get("manualclose")
-
-      Timeout.timeout(10.0) do
-        loop do
-          current_ledger = latest_ledger
-
-          case
-          when current_ledger == next_ledger
-            break
-          when current_ledger > next_ledger
-            raise "whoa! we jumped two ledgers, from #{prev_ledger} to #{current_ledger}"
-          else
-            $stderr.puts "waiting for ledger #{next_ledger}"
-            sleep 0.5
-          end
-        end
-      end
-
-      true
-    end
-
-    Contract String => Any
-    def submit_transaction(envelope_hex)
-      response = server.get("tx", blob: envelope_hex)
-      body = ActiveSupport::JSON.decode(response.body)
-
-      if body["status"] == "ERROR"
-        raise "transaction failed: #{body.inspect}"
-      end
-
-    end
-
-    Contract Stellar::KeyPair => Num
-    def sequence_for(account)
-      row = database[:accounts].where(:accountid => account.address).first
-      row[:seqnum]
-    end
-
-    Contract None => Num
-    def latest_ledger
-      database[:ledgerheaders].max(:ledgerseq)
-    end
-
-    Contract String => String
-    def transaction_result(hex_hash)
-      row = database[:txhistory].where(txid:hex_hash).first
-      row[:txresult]
     end
 
     Contract None => Any
@@ -179,16 +85,6 @@ module StellarCoreCommander
     end
 
     Contract None => Num
-    def http_port
-      base_port
-    end
-
-    Contract None => Num
-    def peer_port
-      base_port + 1
-    end
-
-    Contract None => Num
     def postgres_port
       base_port + 2
     end
@@ -208,24 +104,12 @@ module StellarCoreCommander
       URI.parse(ENV['DOCKER_HOST']).host
     end
 
-    private
     Contract None => String
-    def basename
-      File.basename(working_dir)
+    def http_host
+      docker_host
     end
 
-    Contract String, ArrayOf[String] => Maybe[Bool]
-    def run_cmd(cmd, args)
-      args += [{
-          out: "stellar-core.log", 
-          err: "stellar-core.log",
-        }]
-
-      Dir.chdir working_dir do
-        system(cmd, *args)
-      end
-    end
-
+    private
     def launch_stellar_core
       run_cmd "docker", %W(run
                            --name #{container_name}
--- lib/stellar_core_commander/process.rb
+++ lib/stellar_core_commander/process.rb
@@ -1,6 +1,139 @@
 module StellarCoreCommander
 
   class Process
+    include Contracts
+
+    attr_reader :working_dir
+    attr_reader :base_port
+    attr_reader :identity
+    attr_reader :server
+
+    def initialize(working_dir, base_port, identity)
+      @working_dir = working_dir
+      @base_port   = base_port
+      @identity    = identity
+
+      @server = Faraday.new(url: "http://#{http_host}:#{http_port}") do |conn|
+        conn.request :url_encoded
+        conn.adapter Faraday.default_adapter
+      end
+    end
+
+    Contract None => Any
+    def rm_working_dir
+      FileUtils.rm_rf working_dir
+    end
+
+    Contract None => Any
+    def wait_for_ready
+      loop do
+
+        response = server.get("/info") rescue false
+
+        if response
+          body = ActiveSupport::JSON.decode(response.body)
+
+          break if body["info"]["state"] == "Synced!"
+        end
+
+        $stderr.puts "waiting until stellar-core is synced"
+        sleep 1
+      end
+    end
+
+    Contract None => Bool
+    def close_ledger
+      prev_ledger = latest_ledger
+      next_ledger = prev_ledger + 1
+
+      server.get("manualclose")
+
+      Timeout.timeout(close_timeout) do
+        loop do
+          current_ledger = latest_ledger
+
+          case
+            when current_ledger == next_ledger
+              break
+            when current_ledger > next_ledger
+              raise "whoa! we jumped two ledgers, from #{prev_ledger} to #{current_ledger}"
+            else
+              $stderr.puts "waiting for ledger #{next_ledger}"
+              sleep 0.5
+          end
+        end
+      end
+
+      true
+    end
+
+    Contract None => Num
+    def http_port
+      base_port
+    end
+
+    Contract None => Num
+    def peer_port
+      base_port + 1
+    end
+
+
+
+    Contract String => Any
+    def submit_transaction(envelope_hex)
+      response = server.get("tx", blob: envelope_hex)
+      body = ActiveSupport::JSON.decode(response.body)
+
+      if body["status"] == "ERROR"
+        raise "transaction failed: #{body.inspect}"
+      end
+
+    end
+
+    Contract Stellar::KeyPair => Num
+    def sequence_for(account)
+      row = database[:accounts].where(:accountid => account.address).first
+      row[:seqnum]
+    end
+
+    Contract None => Num
+    def latest_ledger
+      database[:ledgerheaders].max(:ledgerseq)
+    end
+
+    Contract String => String
+    def transaction_result(hex_hash)
+      row = database[:txhistory].where(txid:hex_hash).first
+      row[:txresult]
+    end
+
+    Contract None => String
+    def http_host
+      "127.0.0.1"
+    end
+
+    Contract None => Num
+    def close_timeout
+      5.0
+    end
+
+    private
+    Contract None => String
+    def basename
+      File.basename(working_dir)
+    end
+
+    Contract String, ArrayOf[String] => Maybe[Bool]
+    def run_cmd(cmd, args)
+      args += [{
+                   out: "stellar-core.log",
+                   err: "stellar-core.log",
+               }]
+
+      Dir.chdir working_dir do
+        system(cmd, *args)
+      end
+    end
 
   end
 
