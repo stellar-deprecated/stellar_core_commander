bec0b301152f2e7df26a86b317d309cb70530a6e
Graydon Hoare <graydon@stellar.org>
2015-05-30 16:28:10 -0700

    Try to get multinode quorum/thresh testcase working.

--- examples/multi_host_simple_payment.rb
+++ examples/multi_host_simple_payment.rb
@@ -1,17 +1,14 @@
-process :node1
-process :node2
+process :node1, [:node2, :node3]
+process :node2, [:node1, :node3]
+process :node3, [:node1, :node2]
 
 account :alice
 account :bob
 
-create_account :alice, :master
-create_account :bob, :master
-
-close_ledger
-
 puts "Running txs on node1"
 
 on :node1 do
+  create_account :alice, :master
   create_account :bob, :master
   close_ledger
   payment :master, :bob, [:native, 1000_000000]
