a708349f43db42ea7f679c6d1cfa0123983d60d8
Graydon Hoare <graydon@stellar.org>
2015-06-11 11:11:00 -0700

    Add sketch of multi-version consensus tests.

---
+++ examples/version_mix_consensus.rb
@@ -0,0 +1,42 @@
+old_image = ENV["OLD_IMAGE"]
+new_image = ENV["NEW_IMAGE"]
+
+raise "missing ENV['OLD_IMAGE']" unless old_image
+raise "missing ENV['NEW_IMAGE']" unless new_image
+
+peers = [:oldnode1, :oldnode2, :newnode1, :newnode2]
+
+process :oldnode1, peers, 4, docker_core_image: old_image, docker_pull: true
+process :oldnode2, peers, 4, docker_core_image: old_image, docker_pull: true
+process :newnode1, peers, 4, docker_core_image: new_image, docker_pull: true
+process :newnode2, peers, 4, docker_core_image: new_image, docker_pull: true
+
+account :alice
+account :bob
+
+on :oldnode2 do
+  create_account :alice, :master
+  create_account :bob, :master
+  while not ((account_created :bob) and (account_created :alice))
+    $stderr.puts "Awaiting account-creation"
+    close_ledger
+  end
+  $stderr.puts "oldnode2 bob balance: #{(balance :bob)}"
+  $stderr.puts "oldnode2 alice balance: #{(balance :alice)}"
+  payment :master, :bob, [:native, 1000_000000]
+  close_ledger
+end
+
+on :newnode1 do
+  $stderr.puts "newnode1 bob balance: #{(balance :bob)}"
+  $stderr.puts "newnode1 alice balance: #{(balance :alice)}"
+  raise if (balance :bob) != 11000_000000
+  payment :master, :alice, [:native, 1000_000000]
+  close_ledger
+end
+
+on :oldnode1 do
+  $stderr.puts "oldnode1 bob balance: #{(balance :bob)}"
+  $stderr.puts "oldnode1 alice balance: #{(balance :alice)}"
+  raise if (balance :alice) != 11000_000000
+end
