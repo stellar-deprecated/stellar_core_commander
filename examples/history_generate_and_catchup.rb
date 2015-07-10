199a370c629bf29e65598e3e359e9d84b75bc6f4
Graydon Hoare <graydon@stellar.org>
2015-07-10 12:11:04 -0700

    Freshen examples/history_generate_and_catchup.rb to match acceptance test

--- examples/history_generate_and_catchup.rb
+++ examples/history_generate_and_catchup.rb
@@ -1,16 +1,25 @@
 process :node1, [:node1], 1, accelerate_time: true
 on :node1 do
-  start_load_generation 20000, 20000, 100
-  while ledger_num < 10
+  start_load_generation 2000, 2000, 20
+  retry_until_true retries: 100 do
     $stderr.puts "load generation on node1: ledger #{ledger_num}"
-    sleep 1
+    ledger_num > 10
   end
 end
 
-process :node2, [:node1], 1, forcescp: false, accelerate_time: true, catchup_complete: true
-on :node2 do
-  raise "node2 synced but failed to catch up" if ledger_num < 5
-  while ledger_num < 15
-    $stderr.puts "caught up on node2: ledger #{ledger_num}"
+process :node2_minimal, [:node1], 1, forcescp: false, accelerate_time: true
+on :node2_minimal do
+  retry_until_true retries: 100 do
+    ledger_num > 15
   end
+  check_equal_states [:node2]
+end
+
+
+process :node2_complete, [:node1], 1, forcescp: false, accelerate_time: true, catchup_complete: true
+on :node2_complete do
+  retry_until_true retries: 100 do
+    ledger_num > 15
+  end
+  check_equal_states [:node2]
 end
