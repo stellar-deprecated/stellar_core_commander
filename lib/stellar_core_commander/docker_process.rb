5f2891f7718fe2fdabc311a55c064bceeaa6172e
marta-lokhova <marta@stellar.org>
2018-04-10 15:18:22 -0700

    Added TARGET_PEER_CONNECTION config set to 32

--- lib/stellar_core_commander/docker_process.rb
+++ lib/stellar_core_commander/docker_process.rb
@@ -364,6 +364,8 @@ module StellarCoreCommander
         PREFERRED_PEERS=#{peer_connections}
         VALIDATORS=#{quorum}
 
+        TARGET_PEER_CONNECTION=32
+
         HISTORY_PEERS=#{peer_names}
 
         NETWORK_PASSPHRASE=#{network_passphrase}
